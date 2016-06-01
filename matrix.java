84b5e5aee677d181a6939a4a325960e1bb42ad5b
MauroMrod <mauro.mrod@gmail.com>
2016-06-01 13:41:29 +0100

    mul teste

--- matrix.java
+++ matrix.java
@@ -2,9 +2,18 @@ public class matrix{
 
 	public void calcula(){
 
-		double[][]A= new double[][]{{-3.0,2.0,1.0},{5.0,6.0,7.0}};
-		double[][]B= new double[][]{{5.0,3.0},{7.0,6.0},{3.0,4.0}};
-		double[][]C= new double[][]{{3.0,3.0,3.0},{3.0,2.0,3.0},{4.0,3.0,2.0}};
+		double[][]A= new double[][]{{1.0,2.0,3.0},{4.0,5.0,7.0}};
+		double[][]B= new double[][]{{1.0,2.0},{4.0,5.0},{7.0,8.0}};
+
+		double[][] AmulB = new double[2][2];
+
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				for(int k = 0; k < 3; k++) {
+					AmulB[i][j] += A[i][k] * B[k][j];
+				}
+			}
+		}
 
 	}
 }
--- src/codeGeneration/CodeGenerator.java
+++ src/codeGeneration/CodeGenerator.java
@@ -1,11 +1,8 @@
 package codeGeneration;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +17,75 @@ public class CodeGenerator {
 		this.inputTable = symbolTable;
 		this.generatedFile = new File("Matrix.java");
 		this.outputFile =  new PrintWriter(generatedFile);
+	}
+	
+	public void writeAddTofile(String Matrix1,String Matrix2){
+		
+		int numeroLinhas= inputTable.get(Matrix1).length;
+		int numeroColunas= inputTable.get(Matrix1)[0].length;
+		
+		String newMatrixName= Matrix1+"add"+Matrix2;
+		
+		this.outputFile.write("\n\t\tdouble[][] "+ newMatrixName+" = new double["+numeroLinhas+"]["+numeroColunas+"];\n\n");
+		
+		this.outputFile.write("	\tfor(int c=0;c<"+numeroLinhas+";c++)"
+				+ "{\n"
+				+ "\t\t\tfor(int h=0;h<"+numeroColunas+";h++)"
+				+ "{\n"
+				+ "\t\t\t\t"+newMatrixName+"[c][h]="+Matrix1+"[c][h]+"+Matrix2+"[c][h];\n"
+				+ "\t\t\t}"
+				+ "\n\t\t}");
+		
+		
+		
+	}
+	
+	
+	public void writeSubTofile(String Matrix1,String Matrix2){
+		
+		int numeroLinhas= inputTable.get(Matrix1).length;
+		int numeroColunas= inputTable.get(Matrix1)[0].length;
+		
+		String newMatrixName= Matrix1+"sub"+Matrix2;
+		
+		this.outputFile.write("\n\t\tdouble[][] "+ newMatrixName+" = new double["+numeroLinhas+"]["+numeroColunas+"];\n\n");
+		
+		this.outputFile.write("	\tfor(int c=0;c<"+numeroLinhas+";c++)"
+				+ "{\n"
+				+ "\t\t\tfor(int h=0;h<"+numeroColunas+";h++)"
+				+ "{\n"
+				+ "\t\t\t\t"+newMatrixName+"[c][h]="+Matrix1+"[c][h]-"+Matrix2+"[c][h];\n"
+				+ "\t\t\t}"
+				+ "\n\t\t}");
+		
+		
+		
+	}
+	
+public void writeMulTofile(String Matrix1,String Matrix2) throws Exception{
+		
+		int numeroLinhas1= inputTable.get(Matrix1).length;
+		int numeroColunas1= inputTable.get(Matrix1)[0].length;
+		int numeroLinhas2= inputTable.get(Matrix2).length;
+		
+		int numeroColunas2= inputTable.get(Matrix2)[0].length;
+		
+		if(numeroColunas1 != numeroLinhas2) throw new Exception("Impossivel multiplicar estas matrizes");
+
+		String newMatrixName= Matrix1+"mul"+Matrix2;
+		
+		this.outputFile.write("\n\t\tdouble[][] "+ newMatrixName+" = new double["+numeroLinhas1+"]["+numeroColunas2+"];\n\n");
+		
+		this.outputFile.write("\t\tfor(int i = 0; i < "+numeroLinhas1+"; i++) {\n"
+				+ "\t\t\tfor(int j = 0; j < "+numeroColunas2+"; j++) {\n"
+				+ "\t\t\t\tfor(int k = 0; k < "+numeroColunas1+"; k++) {\n"
+				+ "\t\t\t\t\t"+newMatrixName+"[i][j] += "+Matrix1+"[i][k] * "+Matrix2+"[k][j];\n"
+				+ "\t\t\t\t}\n"
+				+ "\t\t\t}\n"
+				+ "\t\t}\n");
+		
+		 
+		
 	}
 	
 	
@@ -58,12 +124,15 @@ public class CodeGenerator {
 		}
 	}
 	
-	public void generate() {
+	public void generate() throws Exception {
 		 this.outputFile.write("public class matrix{\n\n");
 		
 		 this.outputFile.write("\tpublic void calcula(){\n\n");
 	
 		 initiateVariables();
+		 
+		 //HARCODED
+		 writeMulTofile("A","B");
 		 
 		 this.outputFile.write("\n\t}\n}");
 	}
